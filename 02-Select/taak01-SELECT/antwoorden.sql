2b9e39aa46e1e05c0eda3b2e2afe1d3fc6e4be4f
dylanvaneijk <2114448@talnet.nl>
2020-11-17 23:12:07 +0100

    done

--- 02-Select/taak01-SELECT/antwoorden.sql
+++ 02-Select/taak01-SELECT/antwoorden.sql
@@ -1,13 +1,13 @@
 -- Opdracht 1 - voorbeeld
-SELECT * FROM jaar2016
+SELECT * FROM jaar 2016
 -- Opdracht 2 
-
+SELECT * FROM jaar 2015
 -- Opdracht 3
-
+SELECT country,score FROM jaar 2016
 -- Opdracht 4
-
+SELECT region FROM jaar 2015
 -- Opdracht 5
-
+SELECT region,score FROM jaar 2015
 -- Opdracht 6
+SELECT score,rank FROM jaar 2016
 
--- Opdracht 7
