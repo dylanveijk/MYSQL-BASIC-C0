2892bd6c240b00cb46173dccac5d279ae83ba46c
dylanvaneijk <2114448@talnet.nl>
2020-11-18 19:36:25 +0100

    .

--- 02-Select/taak02-SELECT-WHERE/antwoorden.sql
+++ 02-Select/taak02-SELECT-WHERE/antwoorden.sql
@@ -1,13 +1,13 @@
 -- Opdracht 1 
-
+SELECT * FROM jaar2015 WHERE country="Netherlands"
 -- Opdracht 2 
-
+SELECT rank FROM jaar2016 WHERE country="Netherlands"
 -- Opdracht 3
-
+SELECT score FROM jaar2015 WHERE country="Netherlands"
 -- Opdracht 4
-
+SELECT score FROM jaar2016 WHERE country="Netherlands"
 -- Opdracht 5
-
+SELECT country FROM jaar2015 WHERE region="North America"
 -- Opdracht 6 
 
 -- Opdracht 7 
