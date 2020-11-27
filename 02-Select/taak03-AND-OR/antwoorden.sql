d441c8f3623e752f269ef32827883b00e5490c37
dylanvaneijk <2114448@talnet.nl>
2020-11-27 12:23:52 +0100

    save

--- 02-Select/taak03-AND-OR/antwoorden.sql
+++ 02-Select/taak03-AND-OR/antwoorden.sql
@@ -1,17 +1,17 @@
 -- Opdracht 1 
-
+SELECT name FROM players WHERE nationality= "Spain" AND club = "Chelsea"
 -- Opdracht 2 
-
--- Opdracht 3
-
+SELECT name FROM players WHERE nationality= "Spain" AND age= "17" AND wage= "15000" 3
+-- opdracht 3
+SELECT name FROM players WHERE club= "Liverpool" OR age= ">20"
 -- Opdracht 4
-
+SELECT name from players WHERE nationality="Netherlands" AND club= "ajax"
 -- Opdracht 5
-
+SELECT name from players WHERE club= "ajax" AND NOT nationality= "Netherlands"
 -- Opdracht 6 
-
+SELECT name,age FROM players WHERE club="AZ Alkmaar"
 -- Opdracht 7 
-
+SELECT name,age,club FROM players WHERE club="AZ Alkmaar"
 -- Opdracht 8
 
 -- Opdracht 9
