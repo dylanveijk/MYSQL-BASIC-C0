431ae1a915f7a98167e23b9f050682ebf180cc40
dylanvaneijk <2114448@talnet.nl>
2021-03-11 21:45:24 +0100

    done

--- 03-Delete/taak01-DELETE/antwoorden.sql
+++ 03-Delete/taak01-DELETE/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-
+DELETE FROM players WHERE name = "David Sylva";
 -- Opdracht 2 
-
+DELETE FROM players WHERE club = "Willem II";
 -- Opdracht 3
-
+DELETE FROM players WHERE nationality = "belgium" AND club = "fc barcelona";
 -- Opdracht 4
-
+DELETE FROM players WHERE age >="38"
 -- Opdracht 5
+DELETE FROM players WHERE id ="167905" AND id = "169595"
