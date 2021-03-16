365e8c2b574390fd964892e656956cd21a1ef85a
dylanvaneijk <2114448@talnet.nl>
2021-03-16 13:57:54 +0100

    .

--- opdracht/antwoorden.sql
+++ opdracht/antwoorden.sql
@@ -8,13 +8,13 @@ SELECT NA_Sales FROM videogamesales WHERE genre = "sports"
 -- Opdracht 4
 SELECT name,platform FROM videogamesales WHERE publisher = "nintendo" AND year = "1990-2005"
 -- Opdracht 5
-
+SELECT name,MAX(Global_Sales) FROM videogamesales
 -- Opdracht 6 
-
+SELECT AVG(EU_Sales) FROM videogamesales WHERE genre = "puzzle"
 -- Opdracht 7 
-
+SELECT name,genre,publisher FROM videogamesales WHERE JP_Sales = 532
 -- Opdracht 8
-
+SELECT SUM(Global_Sales) FROM videogamesales WHERE publisher = "nintendo"
 -- Opdracht 9
 
 -- Opdracht 10
