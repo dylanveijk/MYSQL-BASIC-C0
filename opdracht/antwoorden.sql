b05ad1934d5ccb24457990ad1349027bee038e7f
dylanvaneijk <2114448@talnet.nl>
2021-03-16 14:30:40 +0100

    done

--- opdracht/antwoorden.sql
+++ opdracht/antwoorden.sql
@@ -16,15 +16,16 @@ SELECT name,genre,publisher FROM videogamesales WHERE JP_Sales = 532
 -- Opdracht 8
 SELECT SUM(Global_Sales) FROM videogamesales WHERE publisher = "nintendo"
 -- Opdracht 9
-
+SELECT name,year FROM videogamesales WHERE genre = "racing" AND publisher = "nintendo"OR "activision"
 -- Opdracht 10
-
+SELECT AVG(NA_Sales), AVG(EU_Sales), AVG(JP_Sales) FROM videogamesales
 -- Opdracht 11
-
+DELETE FROM videogamesales WHERE name = "halo 2" AND platform = "XB"
 -- Opdracht 12
-
+DELETE FROM videogamesales WHERE year = 2012 OR publisher = "ubisoft"
 -- Opdracht 13
-
+DELETE FROM videogamesales WHERE genre = "adventure" AND  publisher = "nintendo"
 -- Opdracht 14
-
+DELETE FROM videogamesales WHERE publisher = "nintendo" AND Global_Sales = "<1000"
 -- Opdracht 15
+DELETE FROM videogamesales WHERE year = 1997 AND NA_Sales = ">200000"
